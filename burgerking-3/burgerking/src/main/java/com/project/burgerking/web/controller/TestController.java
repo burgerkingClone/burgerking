37eee52b49c9463bef2e154c81a5533e52729893
깃허브 테스트 <>
2023-07-12 09:59:15 +0900

    깃허브 테스트

--- burgerking-3/burgerking/src/main/java/com/project/burgerking/web/controller/TestController.java
+++ burgerking-3/burgerking/src/main/java/com/project/burgerking/web/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.project.burgerking.web.controller;
 
-
+//github 테스트입ㄴ디ㅏ.
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
